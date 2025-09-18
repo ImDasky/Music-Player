750a4720655b92838743c20f3e9c5413905d9647
ImDasky <benjamin2719@gmail.com>
2025-09-18 02:34:14 -0600

    Tab bar: apply dark blur appearance on iOS 15/16 to match iOS 17 look

--- Music/MusicApp.swift
+++ Music/MusicApp.swift
@@ -6,10 +6,29 @@
 //
 
 import SwiftUI
+import UIKit
 
 @main
 struct MusicApp: App {
     let persistenceController = PersistenceController.shared
+
+    init() {
+        // Apply blurred dark appearance for tab bar on iOS 15/16
+        if #available(iOS 17, *) {
+            // iOS 17 has modern blur by default
+        } else {
+            let appearance = UITabBarAppearance()
+            appearance.configureWithDefaultBackground()
+            appearance.backgroundEffect = UIBlurEffect(style: .systemChromeMaterialDark)
+            appearance.backgroundColor = UIColor.black.withAlphaComponent(0.2)
+            UITabBar.appearance().standardAppearance = appearance
+            if #available(iOS 15.0, *) {
+                UITabBar.appearance().scrollEdgeAppearance = appearance
+            }
+            UITabBar.appearance().tintColor = .white
+            UITabBar.appearance().unselectedItemTintColor = UIColor(white: 1.0, alpha: 0.7)
+        }
+    }
 
     var body: some Scene {
         WindowGroup {
